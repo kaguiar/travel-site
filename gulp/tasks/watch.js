05e20d096d36f58ddae646ed0a15f0bde2830bc9
Kevin <kevin.a.aguiar@gmail.com>
2019-02-25 10:08:49 -0500

    Added responsive desgin css, mixins.css

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+﻿var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function () {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    },
+    injectChanges: true,
+    browser: 'microsoft-edge:http://localhost:3000'
+    //browser: 'chrome' //:http://localhost:3000'
+    //browser: 'iexplore' //:http://localhost:3000'
+  });
+
+  watch('./app/index.html', function () {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], function () {
+  return browserSync.reload();
+  //return gulp.src('.app/temp/styles/styles.css')
+  //  .pipe(browserSync.stream({ match: '**/*.css' }));
+});
